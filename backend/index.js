9005c06cf53d1bdc310147857b5ebe432f16f714
imadityagolu <adityasng420.ak@gmail.com>
2025-06-29 15:58:17 +0530

    updated error

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use('/api/user', userRoutes);
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI || `mongodb+srv://adityasng420ak:aditya12345@cluster0.fgxyhi8.mongodb.net/test`;
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
